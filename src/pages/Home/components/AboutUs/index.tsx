dc596cca1a1ae2043a57caffd127cb76a74fbe4d
adautomoises <adautomaleandro@gmail.com>
2023-07-04 22:25:56 -0300

    tentando exibir imagens

--- src/pages/Home/components/AboutUs/index.tsx
+++ src/pages/Home/components/AboutUs/index.tsx
@@ -11,17 +11,17 @@ export function AboutUs() {
       <div className="w-full flex overflow-x-auto gap-2 p-2">
         {[
           [
-            "src/assets/sobre.png",
+            "./src/assets/sobre.png",
             "Lorem Ipsum",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           ],
           [
-            "src/assets/sobre.png",
+            "../src/assets/sobre.png",
             "Lorem Ipsum",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           ],
           [
-            "src/assets/sobre.png",
+            "/src/assets/sobre.png",
             "Lorem Ipsum",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           ],
